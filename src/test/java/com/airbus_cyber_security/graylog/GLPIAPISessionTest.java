32caede08c1eb3b4c852ee5baa220aff08bc1c20
Daisu <daisuki@tuxtrooper.com>
2019-09-17 18:20:27 +0200

    Add test for session token getting through API

--- src/test/java/com/airbus_cyber_security/graylog/GLPIAPISessionTest.java
+++ src/test/java/com/airbus_cyber_security/graylog/GLPIAPISessionTest.java
@@ -1,6 +1,9 @@
 package com.airbus_cyber_security.graylog;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,13 +12,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SessionTest {
-	
-	private Session session;
-	
+public class GLPIAPISessionTest {
+
+	private GLPIAPISession session;
+
 	@Before
 	public void setUp() throws Exception {
-		session = new Session();
+		session = new GLPIAPISession();
 	}
 
 	@After
@@ -28,73 +31,100 @@ public class SessionTest {
 		Map<String, Object> actual = new HashMap<>();
 		Map<String, Object> expected = new HashMap<>();
 		Map<String, Object> apiResult = new HashMap<>();
-		
-	    apiResult.put("1", "xwing.tuxtrooper.com");
-	    apiResult.put("31", "used");
-	    apiResult.put("23", "msi");
-	    apiResult.put("5", "");
-	    apiResult.put("4", "desktop");
-	    apiResult.put("40", null);
-	    apiResult.put("45", null);
-	    apiResult.put("3", "Home");
-	    apiResult.put("19", "2019-09-12 13:59:50");
-	    apiResult.put("17", null);
-	    
-	    expected.put("Name", "xwing.tuxtrooper.com");
-	    expected.put("Status", "used");
-	    expected.put("Manufacturer", "msi");
-	    expected.put("Serialnumber", "");
-	    expected.put("Type", "desktop");
-	    expected.put("Model", null);
-	    expected.put("OSName", null);
-	    expected.put("Location", "Home");
-	    expected.put("Lastupdate", "2019-09-12 13:59:50");
-	    expected.put("Processor", null);
-		
-		actual = session.mappingField(apiResult, Session.getComputerTranslationMatrix(), filter);
-		
+
+		apiResult.put("1", "xwing.tuxtrooper.com");
+		apiResult.put("31", "used");
+		apiResult.put("23", "msi");
+		apiResult.put("5", "");
+		apiResult.put("4", "desktop");
+		apiResult.put("40", null);
+		apiResult.put("45", null);
+		apiResult.put("3", "Home");
+		apiResult.put("19", "2019-09-12 13:59:50");
+		apiResult.put("17", null);
+
+		expected.put("Name", "xwing.tuxtrooper.com");
+		expected.put("Status", "used");
+		expected.put("Manufacturer", "msi");
+		expected.put("Serialnumber", "");
+		expected.put("Type", "desktop");
+		expected.put("Model", null);
+		expected.put("OSName", null);
+		expected.put("Location", "Home");
+		expected.put("Lastupdate", "2019-09-12 13:59:50");
+		expected.put("Processor", null);
+
+		actual = session.mappingField(apiResult, GLPIAPISession.getComputerTranslationMatrix(), filter);
+
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void mappingFieldWithFilterTest() {
-		Session session = new Session();
+		GLPIAPISession session = new GLPIAPISession();
 		String filter = "name,osname";
 		Map<String, Object> actual = new HashMap<>();
 		Map<String, Object> expected = new HashMap<>();
 		Map<String, Object> apiResult = new HashMap<>();
-		
-	    apiResult.put("1", "xwing.tuxtrooper.com");
-	    apiResult.put("31", "used");
-	    apiResult.put("23", "msi");
-	    apiResult.put("5", "");
-	    apiResult.put("4", "desktop");
-	    apiResult.put("40", null);
-	    apiResult.put("45", null);
-	    apiResult.put("3", "Home");
-	    apiResult.put("19", "2019-09-12 13:59:50");
-	    apiResult.put("17", null);
-	    
-	    expected.put("Name", "xwing.tuxtrooper.com");
-	    expected.put("OSName", null);
-		
-		actual = session.mappingField(apiResult, Session.getComputerTranslationMatrix(), filter);
-		
+
+		apiResult.put("1", "xwing.tuxtrooper.com");
+		apiResult.put("31", "used");
+		apiResult.put("23", "msi");
+		apiResult.put("5", "");
+		apiResult.put("4", "desktop");
+		apiResult.put("40", null);
+		apiResult.put("45", null);
+		apiResult.put("3", "Home");
+		apiResult.put("19", "2019-09-12 13:59:50");
+		apiResult.put("17", null);
+
+		expected.put("Name", "xwing.tuxtrooper.com");
+		expected.put("OSName", null);
+
+		actual = session.mappingField(apiResult, GLPIAPISession.getComputerTranslationMatrix(), filter);
+
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void getSessionTokenFromAPITest() {
-		fail("Not yet implemented");
+		GLPIConnection connection = mock(GLPIConnection.class);
+		GLPIAPISession session = new GLPIAPISession();
+		String url = "http://glpi/api";
+		String userToken = "lfljfsfjshufhoqhfoihqushfoiu";
+		StringBuffer response = new StringBuffer();
+		response.append("{\"session_token\":\"o1gcobkuste902s3cflv05ovk5\"}");
+		session.setApiURL(url);
+		session.setUserToken(userToken);
+
+		when(connection.getResponseStream()).thenReturn(response);
+
+		assertEquals("o1gcobkuste902s3cflv05ovk5", session.getSessionTokenFromAPI(connection));
 	}
 	
+	@Test
+	public void getSessionTokenFromAPIFailureTest() {
+		GLPIConnection connection = mock(GLPIConnection.class);
+		GLPIAPISession session = new GLPIAPISession();
+		String url = "http://glpi/api";
+		String userToken = "lfljfsfjshufhoqhfoihqushfoiu";
+		StringBuffer response = new StringBuffer();
+		response.append("{\"session_token_error\":\"o1gcobkuste902s3cflv05ovk5\"}");
+		session.setApiURL(url);
+		session.setUserToken(userToken);
+
+		when(connection.getResponseStream()).thenReturn(response);
+
+		assertEquals("", session.getSessionTokenFromAPI(connection));
+	}
+
 	@Test
 	public void getSearchFromAPITest() {
-		fail("Not yet implemented");
+		fail("Not implemented");
 	}
-	
+
 	@Test
 	public void closeSession() {
-		fail("Not yet implemented");
+		fail("Not implemented");
 	}
 }
