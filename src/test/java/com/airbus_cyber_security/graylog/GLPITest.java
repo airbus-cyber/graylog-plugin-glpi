03e0047d8fba71de60e42851b7ecbd83d9696b7b
Daisu <daisuki@tuxtrooper.com>
2019-09-18 18:01:19 +0200

    Create the GLPITest

---
+++ src/test/java/com/airbus_cyber_security/graylog/GLPITest.java
@@ -0,0 +1,24 @@
+package com.airbus_cyber_security.graylog;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GLPITest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void evaluateTest() {
+		fail("Not yet implemented");
+	}
+
+}
