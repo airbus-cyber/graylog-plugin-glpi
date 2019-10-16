1a04db4ba0ea442ec1e5de6f8b61557902497234
Daisu <daisuki@tuxtrooper.com>
2019-10-16 17:53:49 +0200

    Fix React indent

--- src/web/components/GLPIPluginConfiguration.jsx
+++ src/web/components/GLPIPluginConfiguration.jsx
@@ -13,216 +13,204 @@ import request from 'superagent';
 import UserNotification from 'util/UserNotification';
 
 const GLPIPluginConfiguration = createReactClass({
-  displayName: 'GLPIPluginConfiguration',
+	displayName: 'GLPIPluginConfiguration',
 
-  propTypes: {
-    config: PropTypes.object,
-    updateConfig: PropTypes.func.isRequired,
-  },
+	propTypes: {
+		config: PropTypes.object,
+		updateConfig: PropTypes.func.isRequired,
+	},
 
-  getDefaultProps() {
-    return {
-      config: {
-        glpi_url: 'http://url:port/glpi/apirest.php',
-        api_token: 'api token',
-        heap_size: 100,
-        ttl: 60,
-      },
-    };
-  },
+	getDefaultProps() {
+		return {
+			config: {
+				glpi_url: 'http://url:port/glpi/apirest.php',
+				api_token: 'api token',
+				heap_size: 100,
+				ttl: 60,
+			},
+		};
+	},
 
-  getInitialState() {
-    return {
-      config: ObjectUtils.clone(this.props.config),
-    };
-  },
+	getInitialState() {
+		return {
+			config: ObjectUtils.clone(this.props.config),
+		};
+	},
 
-  componentWillReceiveProps(newProps) {
-    this.setState({ config: ObjectUtils.clone(newProps.config) });
-  },
+	componentWillReceiveProps(newProps) {
+		this.setState({ config: ObjectUtils.clone(newProps.config) });
+	},
 
-  _updateConfigField(field, value) {
-    const update = ObjectUtils.clone(this.state.config);
-    update[field] = value;
-    this.setState({ config: update });
-  },
+	_updateConfigField(field, value) {
+		const update = ObjectUtils.clone(this.state.config);
+		update[field] = value;
+		this.setState({ config: update });
+	},
 
-  _onCheckboxClick(field, ref) {
-    return () => {
-      this._updateConfigField(field, this.refs[ref].getChecked());
-    };
-  },
+	_onCheckboxClick(field, ref) {
+		return () => {
+			this._updateConfigField(field, this.refs[ref].getChecked());
+		};
+	},
 
-  _onSelect(field) {
-    return (selection) => {
-      this._updateConfigField(field, selection);
-    };
-  },
+	_onSelect(field) {
+		return (selection) => {
+			this._updateConfigField(field, selection);
+		};
+	},
 
-  _onUpdate(field) {
-    return e => {
-      this._updateConfigField(field, e.target.value);
-    };
-  },
+	_onUpdate(field) {
+		return e => {
+			this._updateConfigField(field, e.target.value);
+		};
+	},
 
-  _openModal() {
-    this.refs.glpiConfigModal.open();
-  },
+	_openModal() {
+		this.refs.glpiConfigModal.open();
+	},
 
-  _closeModal() {
-    this.refs.glpiConfigModal.close();
-  },
+	_closeModal() {
+		this.refs.glpiConfigModal.close();
+	},
 
-  _resetConfig() {
-    // Reset to initial state when the modal is closed without saving.
-    this.setState(this.getInitialState());
-  },
+	_resetConfig() {
+		// Reset to initial state when the modal is closed without saving.
+		this.setState(this.getInitialState());
+	},
 
-  _saveConfig() {
-    this.props.updateConfig(this.state.config).then(() => {
-      this._closeModal();
-    });
-  },
-  
-  _test_connection() {
-      request
-            .get(this.state.config.glpi_url + "/initSession")
-            .set('Authorization', 'user_token ' + this.state.config.api_token)
-            .set('Content-Type', 'application/json')
-            .end((err, resp) => {
-                if (!err) {
-                    UserNotification.success("Connection to "
-                            + this.state.config.glpi_url + " with token "
-                            + this.state.config.api_token + " succeed", "Succeed connection")
-                }
-                else {
-                    UserNotification.error("Impossible to connect to "
-                            + this.state.config.glpi_url + " with token "
-                            + this.state.config.api_token + " and error: " + err, "Failed to connect");
-                }
-            })
-  },
+	_saveConfig() {
+		this.props.updateConfig(this.state.config).then(() => {
+			this._closeModal();
+		});
+	},
 
-  render() {
-    return (
-      <div>
-        <h3>GLPI Plugin Configuration</h3>
+	_test_connection() {
+		request
+			.get(this.state.config.glpi_url + "/initSession")
+			.set('Authorization', 'user_token ' + this.state.config.api_token)
+			.set('Content-Type', 'application/json')
+			.end((err, resp) => {
+				if (!err) {
+					UserNotification.success("Connection to "
+						+ this.state.config.glpi_url + " with token "
+						+ this.state.config.api_token + " succeed", "Succeed connection")
+				}
+				else {
+					UserNotification.error("Impossible to connect to "
+						+ this.state.config.glpi_url + " with token "
+						+ this.state.config.api_token + " and error: " + err, "Failed to connect");
+				}
+			})
+	},
 
-        <p>
-          Base configuration GLPI plugin (URL and API key). Note that some parameters will be stored 
-          in MongoDB without encryption. Graylog users with required permissions will be able 
-          to read them in the configuration dialog on this page.
-        </p>
+	render() {
+		return (
+			<div>
+				<h3>GLPI Plugin Configuration</h3>
 
-        <dl className="deflist">
-          <dt>GLPI URL:</dt>
-          <dd>
-            {this.state.config.glpi_url
-              ? this.state.config.glpi_url
-              : '[not set]'}
-          </dd>
+				<p>
+					Base configuration GLPI plugin (URL and API key). Note that some parameters will be stored
+					in MongoDB without encryption. Graylog users with required permissions will be able
+					to read them in the configuration dialog on this page.</p>
 
-          <dt>API Token:</dt>
-          <dd>
-            {this.state.config.api_token 
-              ? this.state.config.api_token
-              : '[not set]'}
-          </dd>
-            
-          <dt>Cache heap size:</dt>
-          <dd>
-            {this.state.config.heap_size
-              ? this.state.config.heap_size
-              : '[not set]'}
-          </dd>
-          
-          <dt>Cache TTL:</dt>
-          <dd>
-            {this.state.config.ttl
-              ? this.state.config.ttl
-              : '[not set]'}
-          </dd>
-        </dl>
+				<dl className="deflist">
+					<dt>GLPI URL:</dt>
+					<dd>
+						{this.state.config.glpi_url
+							? this.state.config.glpi_url
+							: '[not set]'}
+					</dd>
 
-        <IfPermitted permissions="clusterconfigentry:edit">
-          <Button bsStyle="info" bsSize="xs" onClick={this._openModal}>
-            Configure
-          </Button>
-        </IfPermitted>
+					<dt>API Token:</dt>
+					<dd>
+						{this.state.config.api_token
+							? this.state.config.api_token
+							: '[not set]'}
+					</dd>
 
-        <BootstrapModalForm
-          ref="glpiConfigModal"
-          title="Update GLPI Plugin Configuration"
-          onSubmitForm={this._saveConfig}
-          onModalClose={this._resetConfig}
-          submitButtonText="Save">
-          <fieldset>
+					<dt>Cache heap size:</dt>
+					<dd>
+						{this.state.config.heap_size
+							? this.state.config.heap_size
+							: '[not set]'}
+					</dd>
 
-            <Input
-              id="glpi-url"
-              type="text"
-              label="GLPI URL"
-              help={
-                <span>
-                  GLPI instance URL (http://url:port/glpi/apirest.php).
-                </span>
-              }
-              name="glpi_url"
-              value={this.state.config.glpi_url}
-              onChange={this._onUpdate('glpi_url')}
-            />
+					<dt>Cache TTL:</dt>
+					<dd>
+						{this.state.config.ttl
+							? this.state.config.ttl
+							: '[not set]'}
+					</dd>
+				</dl>
 
-            <Input
-              id="api-token"
-              type="text"
-              label="GLPI API Token"
-              help={
-                <span>
-                  Note that this will be stored in plaintext. Please consult the documentation for
-                  suggested rights to assign to the underlying IAM user.
-                </span>
-              }
-              name="api_token"
-              value={this.state.config.api_token}
-              onChange={this._onUpdate('api_token')}
-            />
-            
-            <Input
-              id="heap-size"
-              type="text"
-              label="Cache Heap Size (Mib)"
-              help={
-                <span>
-                  Cache size in Mib. Graylog service restart is needed after change.
-                </span>
-              }
-              name="heap_size"
-              value={this.state.config.heap_size}
-              onChange={this._onUpdate('heap_size')}
-            />
-          
-            <Input
-              id="ttl"
-              type="text"
-              label="Cache TTL (seconds)"
-              help={
-                <span>
-                  Cache TTL in seconds. Graylog service restart is needed after change.
-                </span>
-              }
-              name="ttl"
-              value={this.state.config.ttl}
-              onChange={this._onUpdate('ttl')}
-            />
-            
-            <Button bsStyle="info" bsSize="s" onClick={this._test_connection}>
-              Test
-            </Button>
-          </fieldset>
-        </BootstrapModalForm>
-      </div>
-    );
-  },
+				<IfPermitted permissions="clusterconfigentry:edit">
+					<Button bsStyle="info" bsSize="xs" onClick={this._openModal}>Configure</Button>
+				</IfPermitted>
+
+				<BootstrapModalForm
+					ref="glpiConfigModal"
+					title="Update GLPI Plugin Configuration"
+					onSubmitForm={this._saveConfig}
+					onModalClose={this._resetConfig}
+					submitButtonText="Save">
+					<fieldset>
+
+						<Input
+							id="glpi-url"
+							type="text"
+							label="GLPI URL"
+							help={
+								<span>GLPI instance URL (http://url:port/glpi/apirest.php).</span>
+							}
+							name="glpi_url"
+							value={this.state.config.glpi_url}
+							onChange={this._onUpdate('glpi_url')}
+						/>
+
+						<Input
+							id="api-token"
+							type="text"
+							label="GLPI API Token"
+							help={
+								<span>Note that this will be stored in plaintext.
+								Please consult the documentation for suggested rights to
+								assign to the underlying IAM user.</span>
+							}
+							name="api_token"
+							value={this.state.config.api_token}
+							onChange={this._onUpdate('api_token')}
+						/>
+
+						<Input
+							id="heap-size"
+							type="text"
+							label="Cache Heap Size (Mib)"
+							help={
+								<span> Cache size in Mib. Graylog service restart is needed after change.</span>
+							}
+							name="heap_size"
+							value={this.state.config.heap_size}
+							onChange={this._onUpdate('heap_size')}
+						/>
+
+						<Input
+							id="ttl"
+							type="text"
+							label="Cache TTL (seconds)"
+							help={
+								<span>Cache TTL in seconds. Graylog service restart is needed after change.</span>
+							}
+							name="ttl"
+							value={this.state.config.ttl}
+							onChange={this._onUpdate('ttl')}
+						/>
+
+						<Button bsStyle="info" bsSize="s" onClick={this._test_connection}>Test</Button>
+					</fieldset>
+				</BootstrapModalForm>
+			</div>
+		);
+	},
 });
 
 export default GLPIPluginConfiguration;
